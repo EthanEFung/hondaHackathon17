2a718cd4d5d1f164fa20034c76cb35ba1b69f749
Ethan Fung <EthanEFung@gmail.com>
2017-11-28 13:16:51 -0800

    feat: add boilerplate for future testing

---
+++ test/roadCoordinateController.test.js
@@ -0,0 +1,4 @@
+describe("the road coordinate controller", function() {
+  it("should receive road coordinates", function() {});
+  it("should it should be able to send data to python scripts", function() {});
+});
