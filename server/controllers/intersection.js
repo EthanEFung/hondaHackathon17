75a8aa89c5ec2eebfe8a479994f3c27dba2c9228
Ethan Fung <EthanEFung@gmail.com>
2017-11-28 09:27:05 -0800

    feat: getCollisionScore works

--- server/controllers/intersection.js
+++ server/controllers/intersection.js
@@ -1,26 +1,24 @@
-/** CURRENT BUG
- * detailed below is the issue I am facing
- */
-const spawn = require("child_process").spawn;
-const py = spawn("python", ["../sampleModel.py"]); //sampleModel is found in the server folder
+const { spawn, spawnSync } = require("child_process");
+const py = spawn("python", ["server/sampleModel.py"]);
 
-let data = [1, 2, 3, 4, 5]; //here is the data I want to process
-let dataString = ""; //this is where the data from python should be after processing
-
-py.stdout.on("data", function(data) {
-  console.log("recieving data", data); //this print is never run in the terminal
-  dataString += data.toString();
-});
-py.stdout.on("end", function() {
-  console.log("received", dataString); //this is run with the empty string defined above
-});
-py.stdin.write(JSON.stringify(data)); //ideally this function is invoked and written to python
-py.stdin.end(); //ends the process
+let data = [1, 2, 3, 4, 5, 6, 7, 8];
+let dataString = "";
 
 module.exports = {
   getCollisionScore: (req, res) => {
-    console.log(dataString);
     const { intersection, long, lat } = req.params;
-    res.send(dataString);
+
+    py.stdout.on("data", data => {
+      console.log("recieving data", data);
+      dataString += data.toString();
+    });
+    py.stdout.on("end", function() {
+      res.send(dataString);
+    });
+    py.stderr.on("data", err => {
+      if (err) throw "Error" + err;
+    });
+    py.stdin.write(JSON.stringify(data));
+    py.stdin.end();
   }
 };
